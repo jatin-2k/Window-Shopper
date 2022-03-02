133c2a0f664186f5e53b6c0addef0cc8fe1fa715
Jatin Agrawal <jatinagrawal2000@gmail.com>
2022-03-02 12:47:27 +0530

    Ios input Accessory and IBDesignables

--- Window-Shopper/Controllers/MainVC.swift
+++ Window-Shopper/Controllers/MainVC.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var First: CurrencyTextField!
+    @IBOutlet weak var Second: CurrencyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.Calculate), for: .touchUpInside)
+        
+        First.inputAccessoryView = calcBtn
+        Second.inputAccessoryView = calcBtn
     }
-
+    
+    @objc func Calculate(){
+        print("I Here")
+    }
 
 }
 
--- Window-Shopper/Views/CurrencyTextField.swift
+++ Window-Shopper/Views/CurrencyTextField.swift
@@ -7,10 +7,18 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    
+    func customizeView(){
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.2490167942)
         layer.cornerRadius = 5.0
         textAlignment = .center
@@ -20,5 +28,4 @@ class CurrencyTextField: UITextField {
         attributedPlaceholder = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) ])
         textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
-
 }
